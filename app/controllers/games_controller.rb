594c8705ab2e66eff1b4c63f0b4371e4ca6b8aaa
John <jjh5166@gmail.com>
2018-03-14 17:17:47 -0400

    msf authenticate global

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,23 +1,20 @@
 class GamesController < ApplicationController
-  USERNAME = open('lib/assets/.username').first.chomp
-  PASSWORD = open('lib/assets/.password').first.chomp
+
 def show
 
 end
 
 def season
-  msf = MySportsFeeds.new(version="1.2", true)
-  msf.authenticate(USERNAME, PASSWORD)
-  @games = msf.msf_get_data('nba', '2016-2017-regular', 'full_game_schedule', 'json')["fullgameschedule"]["gameentry"]
+
+  @games = $msf.msf_get_data('nba', '2016-2017-regular', 'full_game_schedule', 'json')["fullgameschedule"]["gameentry"]
 end
 def scoreboard
-msf = MySportsFeeds.new(version="1.2", true)
-msf.authenticate(USERNAME, PASSWORD)
-@games = msf.msf_get_data(params[:league], '2016-2017-regular', 'scoreboard', 'json', 'fordate' => params[:date])['scoreboard']['gameScore']
+
+@games = $msf.msf_get_data(params[:league], '2016-2017-regular', 'scoreboard', 'json', 'fordate' => params[:date])['scoreboard']['gameScore']
 end
 def boxscore
 
 
-  
+
 end
 end
