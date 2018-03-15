80beb559236ef1ed17b5eec87cd4dfdcf9d7ea4c
Brandon P <brandonpegula@gmail.com>
2018-03-15 10:31:05 -0400

    Navbar edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'games#scoreboard'
+root to: 'games#season'
 
 get '/:league/scoreboard/:date', to: 'games#scoreboard'
 end
