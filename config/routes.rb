754faf5309ab2ea4ff94e4ab4d6285fb7ca7b6f4
Brandon P <brandonpegula@gmail.com>
2018-03-16 13:13:51 -0400

    NBA Linked

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'league#index'
+root to: 'leagues#index'
 
 get '/:league/scoreboard/:date', to: 'games#scoreboard'
 
 get '/:league/boxscore/:gameid', to: 'games#boxscore' 
 
-get '/:league/schedule/:Abbreviation', to: "leagues#teamseason"
+get '/:league/schedule/:Abbreviation', to: "leagues#teamseason", as: "leagues_teamseason"
 
-get '/:league/:season', to:"leagues#season"
+get '/:league/:season', to:"leagues#season", as:"leagues_season"
 
 
 end
