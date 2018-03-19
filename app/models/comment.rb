8af080c67cd184df398307f40ac9773715ebebf4
John <jjh5166@gmail.com>
2018-03-19 15:26:13 -0400

    comments controller, db updated

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :picture
-  validates :game_id, presence: true
+  validates :picture_id, presence: true
   validates :user_id, presence: true
   validates :content, presence: true, length:{ in: 2..150}
 end
