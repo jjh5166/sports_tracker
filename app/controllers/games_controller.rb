0d7f2ac475f462e5a9bc1dbe5e6e6ea207a81fb7
John <jjh5166@gmail.com>
2018-03-20 12:55:36 -0400

    comments on modal with js

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,16 @@ class GamesController < ApplicationController
 
 	def boxscore
 		@games = $msf.msf_get_data(params[:league], params[:season], 'game_boxscore', 'json', 'gameid' => params[:gameid])['gameboxscore']
-		@pics = Picture.where(game_id: params[:gameid])
+		@pics = Picture.where(game_id: params[:gameid]).reverse
+
+
+		@comments = {}
+
+		@pics.first(8).each do |h|
+			@comments[h.id] = []
+			Comment.where(picture_id: h.id).each do |c|
+				@comments[h.id].push(c.content)
+			end
+		end
 	end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,6 +5,6 @@ class Picture < ApplicationRecord
   validates :user_id, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200", box: "150x150#" }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
