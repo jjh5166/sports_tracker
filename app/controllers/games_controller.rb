c92fbb965f646fb8c3ae3b81c706e43b800129d6
John <jjh5166@gmail.com>
2018-03-21 15:31:17 -0400

    logged out modal fixes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,8 +8,14 @@ class GamesController < ApplicationController
 		@games = $msf.msf_get_data(params[:league], params[:season], 'game_boxscore', 'json', 'gameid' => params[:gameid])['gameboxscore']
 		@pics = Picture.where(game_id: params[:gameid]).reverse
 
+		if user_signed_in?
+			@user_id = current_user.id
+		else
+			@user_id = "NO_USER"
+		end
+
 		@comments = {}
-    
+
 		@pics.first(8).each do |h|
 			@comments[h.id] = [[],[]]
 			Comment.where(picture_id: h.id).each do |c|
