25a7f016d240289fde15747c99e05aed6f715df6
Brandon P <brandonpegula@gmail.com>
2018-04-18 15:56:53 -0400

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,15 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_region => ENV['AWS_REGION']
+  },
+  :url => ':s3_domain_url',
+  :path => "/:class/:attachment/:id_partition/:style/:filename"
 }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
